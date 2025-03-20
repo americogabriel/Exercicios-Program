307e00f67ef3a36913f3824f691fffcf948d9f72
americogabriel <gabrieldouble007@gmail.com>
2025-03-20 17:34:28 -0300

    Update exerciciosalario.py 0 no exist

--- exerciciosalario.py
+++ exerciciosalario.py
@@ -1,5 +1,1 @@
-salario = float (input('qual o seu salario mensal?'))
-aumento = salario * 0.25
-novosalario = salario + aumento
 
-print ('Após o aumento do salário, seu novo valor salarial será: R$',novosalario)
