7c503a43934212b05033a1466da51d9f0bf24948
americogabriel <gabrieldouble007@gmail.com>
2025-03-20 17:32:59 -0300

    Exercicio do Salario
    Eexercicio que mostra um salario após um aumento de 25%

---
+++ exerciciosalario.py
@@ -0,0 +1,5 @@
+salario = float (input('qual o seu salario mensal?'))
+aumento = salario * 0.25
+novosalario = salario + aumento
+
+print ('Após o aumento do salário, seu novo valor salarial será: R$',novosalario)
